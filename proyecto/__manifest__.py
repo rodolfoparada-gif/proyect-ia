fd0bbddf746a48fa73cb5416f60a8c26a68dd83b
Rodolfo Parada <rodolfo.parada@sellside.cl>
2026-01-28 20:11:52 -0300

    fix manifiesto

--- proyecto/__manifest__.py
+++ proyecto/__manifest__.py
@@ -1,21 +1,16 @@
 {
     'name': 'IA Editor Pro',
     'version': '1.0',
+    'summary': 'Generador de contenido inteligente',
+    'category': 'Extra Tools', # Cambia a esta categoría oficial
     'author': 'Rodolfo Parada',
-    'category': 'Productivity', # Categoría estándar para que sea fácil de encontrar
-    'summary': 'Solución de Inteligencia Artificial para gestión de perfiles',
-    'description': """
-        App profesional para integrar OpenAI con Odoo.
-        Permite generar y mejorar descripciones automáticamente.
-    """,
     'depends': ['base', 'mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/ai_view.xml',
     ],
     'installable': True,
-    'application': True,    # OBLIGATORIO para que aparezca en el tablero de Apps
-    'auto_install': False,
+    'application': True,    # ESTO HACE QUE APAREZCA EL BOTÓN INSTALAR
+    'auto_install': False,  # Evita que se instale solo
     'license': 'LGPL-3',
-    'icon': '/proyecto/static/description/icon.png', # Ruta al icono de tu app
 }

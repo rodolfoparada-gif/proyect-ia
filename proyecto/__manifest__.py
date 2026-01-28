3c6e923396c103777bfa7cb0737844edc1f1d683
Rodolfo Parada <rodolfo.parada@sellside.cl>
2026-01-28 20:37:31 -0300

    fix manifest3

--- proyecto/__manifest__.py
+++ proyecto/__manifest__.py
@@ -1,15 +1,17 @@
 {
     'name': 'IA Editor Pro',
     'version': '1.0',
-    'category': 'Sales', # Cámbialo a 'Sales' temporalmente para que Odoo lo reconozca como App de negocio
+    'summary': 'Generador de contenido inteligente',
+    'category': 'Extra Tools', # Cambia a esta categoría oficial
     'author': 'Rodolfo Parada',
-    'depends': ['base', 'mail'],
+    'depends': ['base', 'mail','contacts'],
     'data': [
         'security/ir.model.access.csv',
         'views/ai_view.xml',
     ],
     'installable': True,
-    'application': True,    # ESTO ES LO QUE GENERA EL BOTÓN
-    'auto_install': False,
+    'application': True,    # ESTO HACE QUE APAREZCA EL BOTÓN INSTALAR
+    'auto_install': False,  # Evita que se instale solo
     'license': 'LGPL-3',
 }
+

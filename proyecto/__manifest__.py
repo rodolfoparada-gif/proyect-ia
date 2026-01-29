5b3d2cb7ec487240799f2ea54c263caf92dbe327
Rodolfo Parada <rodolfo.parada@sellside.cl>
2026-01-28 21:16:45 -0300

    fix modificacion en archivos manifest y views

--- proyecto/__manifest__.py
+++ proyecto/__manifest__.py
@@ -1,17 +1,16 @@
 {
     'name': 'IA Editor Pro',
     'version': '1.0',
-    'summary': 'Generador de contenido inteligente',
-    'category': 'Productivity', # Cambia a esta categoría oficial
+    'summary': 'Generador de contenido con IA',
+    'category': 'Productivity',
     'author': 'Rodolfo Parada',
-    'depends': ['base', 'mail','contacts'],
+    'depends': ['base', 'mail', 'contacts'], # Indispensable para heredar vistas de contactos
     'data': [
         'security/ir.model.access.csv',
         'views/ai_view.xml',
     ],
     'installable': True,
-    'application': True,    # ESTO HACE QUE APAREZCA EL BOTÓN INSTALAR
-    'auto_install': False,  # Evita que se instale solo
+    'application': True,    # Esto habilita el botón Instalar
+    'auto_install': False,
     'license': 'LGPL-3',
 }
-

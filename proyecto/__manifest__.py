c4a7642c9866ecadaf4bd755f5178df23806bfad
Rodolfo Parada <rodolfo.parada@sellside.cl>
2026-01-29 01:12:28 -0300

    fix

--- proyecto/__manifest__.py
+++ proyecto/__manifest__.py
@@ -1,14 +1,30 @@
 
+# {
+#     'name': 'IA Editor Pro',
+#     'version': '1.2',
+#     'summary': 'Editor de documentos independiente con IA',
+#     # 'category': 'Tools',
+#      'category': 'Productivity',
+#     'author': 'Rodolfo Parada',
+#     'depends': ['base','web'], # Eliminamos 'contacts'
+#     'data': [
+#         'security/ir.model.access.csv',
+#         'views/ai_view.xml',
+#     ],
+#     'installable': True,
+#     'application': True,
+#     'license': 'LGPL-3',
+# }
+
 {
     'name': 'IA Editor Pro',
     'version': '1.2',
     'summary': 'Editor de documentos independiente con IA',
-    # 'category': 'Tools',
-     'category': 'Productivity',
+    'category': 'Tools',
     'author': 'Rodolfo Parada',
-    'depends': ['base','web'], # Eliminamos 'contacts'
+    'depends': ['base', 'web'], # 'web' es vital en producción
     'data': [
-        'security/ir.model.access.csv',
+        'security/ir.model.accessgit.csv',
         'views/ai_view.xml',
     ],
     'installable': True,
